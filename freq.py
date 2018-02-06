83f91e86614cf2492c52b496628793c8fcbfd0b5
minasso <andtas@gmail.com>
2018-02-06 07:12:03 -0500

    fixed word boundary issue, added missing songs

--- freq.py
+++ freq.py
@@ -1,12 +1,13 @@
 import re
 import string
-frequency = {}
+
 document_text = open('lyrics.txt', 'r')
 text_string = document_text.read().lower()
-text_string.replace(',','')
-# words='kill,'
+text_string = text_string.replace(',','')
+
 match_pattern = re.findall(r'\bdie|death|dead|dying|kill|hell|evil\b', text_string)
  
+frequency = {}
 for word in match_pattern:
     count = frequency.get(word,0)
     frequency[word] = count + 1
